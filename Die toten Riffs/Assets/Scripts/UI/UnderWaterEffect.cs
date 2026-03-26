9e660d331dd1640d0a65cd2160fc585f7f30af09
StanTheMove <stas.kholopkin@gmail.com>
2026-03-26 14:13:38 +0200

    Added UnderWaterEffect script
    For optimization, I will use just an image.

---
+++ Die toten Riffs/Assets/Scripts/UI/UnderWaterEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace UI
+{
+    public class UnderWaterEffect : MonoBehaviour
+    {
+        private Image waterOverlay;
+
+        private void Start()
+        {
+            if (waterOverlay != null) waterOverlay.enabled = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("Water") && waterOverlay != null)
+                waterOverlay.enabled = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if(other.CompareTag("Water") && waterOverlay != null)
+                waterOverlay.enabled = false;
+        }
+    }
+}
